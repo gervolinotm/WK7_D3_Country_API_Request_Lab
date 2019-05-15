1cb2623be3aa362140ee60eb67fa8350facb3c9a
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-05-15 13:42:48 +0100

    request_helper.js added

---
+++ lab_requests_countries_start/src/models/countries.js
@@ -0,0 +1,25 @@
+const RequestHelper = function(url){
+   this.url = url;
+ };
+
+RequestHelper.prototype.get = function (onComplete){
+   const xhr = new XMLHttpRequest()
+
+   console.log("Away getting data")
+
+   xhr.addEventListener('load', () => {
+     if (xhr.status !== 200){
+       return
+     }
+     const jsonString = (xhr.responseText);
+     const data = JSON.parse(jsonString)
+     onComplete(data)
+   })
+
+   xhr.open('GET', this.url)
+   xhr.setRequestHeader('Accept', 'application/json')
+   xhr.send()
+
+}
+
+module.exports = RequestHelper;

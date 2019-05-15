dc24059c25b52f64f086bd4c8161c110036fa384
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-05-15 14:49:39 +0100

    Broadcasting change from SelectView

--- lab_requests_countries_start/src/views/select_view.js
+++ lab_requests_countries_start/src/views/select_view.js
@@ -8,7 +8,24 @@ SelectView.prototype.bindEvents = function () {
   PubSub.subscribe('Country:all-countries-ready', (event) =>{
     const allCountries = event.detail
     console.log(allCountries);
+    this.populate(allCountries)
   });
+
+  this.element.addEventListener('change', (event) => {
+    const selectedIndex = event.target.value
+    console.log(selectedIndex)
+    PubSub.publish('SelectView:change', selectedIndex)
+  })
 }
+
+
+SelectView.prototype.populate = function (allCountriesData) {
+  allCountriesData.forEach((country, index) => {
+    const option = document.createElement('option');
+    option.textContent = country.name;
+    option.value = index;
+    this.element.appendChild(option);
+  });
+};
 
 module.exports = SelectView;
